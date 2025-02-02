3e208ac87a6f5b96d09e70659dff9a465d522617
Giorgi JiJiashvili <g.jijiashvili@myjobs.ge>
2025-02-02 14:28:25 +0400

    axios api

--- app/api/authService.tsx
+++ app/api/authService.tsx
@@ -4,9 +4,28 @@ import { loginData, resetPassword, PaymentData } from "./apiTypes";
 require('dotenv').config()
 
 
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASEAPIURL,
+});
+
+api.interceptors.response.use(
+  (response) => response, // Return response if successful
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("JWT expired. Logging out...");
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("userId");
+      window.location.href = "/login"; // Redirect to login page
+    }
+    return Promise.reject(error);
+  }
+);
+
+
 export async function getUserData(data: loginData) {
     try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/login` , data)
+        const response = await api.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/login` , data)
         return response;
     } catch (err) {
         throw err
@@ -16,7 +35,7 @@ export async function getUserData(data: loginData) {
 
   export async function postRegisterData(data: loginData) {
     try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/register` , data)
+        const response = await api.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/register` , data)
         return response;
     } catch (err) {
         throw err
@@ -25,7 +44,7 @@ export async function getUserData(data: loginData) {
   
   export async function resetUserPassword(email: resetPassword) {
     try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/reset-password` , email)
+        const response = await api.post(`${process.env.NEXT_PUBLIC_BASEAPIURL}/reset-password` , email)
         return response;
     } catch (err) {
         throw err
@@ -36,7 +55,7 @@ export async function getUserData(data: loginData) {
     const accessToken = localStorage?.getItem("accessToken");
 
     try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_BASEAPIURL}/self/${id}`, {
+        const response = await api.get(`${process.env.NEXT_PUBLIC_BASEAPIURL}/self/${id}`, {
             headers: {
                 Authorization: accessToken
             }
@@ -53,7 +72,7 @@ export async function uploadPhoto(id: string | null, photo: FormData) {
     const accessToken = localStorage?.getItem("accessToken");
 
   try {
-    const response = await axios.post(
+    const response = await api.post(
       `${process.env.NEXT_PUBLIC_BASEAPIURL}/uploadPhoto/${id}`,
       photo, {
         headers: {
@@ -72,7 +91,7 @@ export async function deletePhoto(id: string | null) {
     const accessToken = localStorage?.getItem("accessToken");
 
   try {
-    const response = await axios.delete(
+    const response = await api.delete(
       `${process.env.NEXT_PUBLIC_BASEAPIURL}/deletePhoto/${id}`, {
         headers: {
             Authorization: accessToken
@@ -91,7 +110,7 @@ export async function payment(data: PaymentData) {
   const accessToken = localStorage?.getItem("accessToken");
 
 try {
-  const response = await axios.post(
+  const response = await api.post(
     `${process.env.NEXT_PUBLIC_BASEAPIURL}/pay/`, data, {
       headers: {
           Authorization: accessToken
@@ -111,7 +130,7 @@ export async function getMyOrders() {
   const accessToken = localStorage?.getItem("accessToken");
 
 try {
-  const response = await axios.get(
+  const response = await api.get(
     `${process.env.NEXT_PUBLIC_BASEAPIURL}/myorders`, {
       headers: {
           Authorization: accessToken
@@ -130,7 +149,7 @@ try {
 export async function getProducts() {
 
 try {
-  const response = await axios.get(
+  const response = await api.get(
     `${process.env.NEXT_PUBLIC_BASEAPIURL}/products`
   );
   return response;
@@ -145,7 +164,7 @@ export async function updateOrder(session_id: string) {
   const accessToken = localStorage?.getItem("accessToken");
 
 try {
-  const response = await axios.get(
+  const response = await api.get(
     `${process.env.NEXT_PUBLIC_BASEAPIURL}/orders/${session_id}`, {
       headers: {
           Authorization: accessToken
